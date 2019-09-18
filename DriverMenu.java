becc994a5eba5b6be741174d0baf2ac18335402a
hengkysuryaa <hengkysurya88@gmail.com>
2019-09-18 20:33:42 +0700

    class menu

---
+++ DriverMenu.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class DriverMenu {
+    public static void main(String[] args) {
+
+        menu M = new menu ();
+
+        do {
+
+            M.ListMenu();
+            M.SubMenu();
+
+        } while 
+       
+    }
+}
