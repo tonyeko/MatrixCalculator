c4b3a4e7c98ca3df357fb36900e779b2bfcfb622
hengkysuryaa <hengkysurya88@gmail.com>
2019-09-18 20:52:57 +0700

    Menu looping

--- DriverMenu.java
+++ DriverMenu.java
@@ -3,14 +3,28 @@ import java.util.Scanner;
 class DriverMenu {
     public static void main(String[] args) {
 
+        // deklarasi variabel
+        int PilihanMenu,PilihanSubMenu;
+
+        // membuat scanner baru
+        Scanner input = new Scanner(System.in);
+
         menu M = new menu ();
 
         do {
 
             M.ListMenu();
-            M.SubMenu();
+            System.out.print("Pilihan Anda: ");
+            PilihanMenu = input.nextInt();
+            if (PilihanMenu == 1 || PilihanMenu == 2 || PilihanMenu == 3)
+            {
+                System.out.println("");
+                M.SubMenu();
+                System.out.print("Pilihan Anda: ");
+                PilihanSubMenu = input.nextInt();
+            }     
 
-        } while 
+        } while (PilihanMenu != 7);
        
     }
 }
--- menu.java
+++ menu.java
@@ -1,8 +1,9 @@
 class menu {
 
     menu () {}
-    
+
     void ListMenu() {
+        System.out.println("");
         System.out.println("MENU");
         System.out.println("1. Sistem Persamaan Linear");
         System.out.println("2. Determinan");
@@ -11,11 +12,13 @@ class menu {
         System.out.println("5. Adjoin");
         System.out.println("6. Interpolasi Polinom");
         System.out.println("7. Exit");
+        System.out.println("");
     }
     void SubMenu(){
         System.out.println("1. Metode eliminasi Gauss");
         System.out.println("2. Metode eliminasi Gauss-Jordan");
         System.out.println("3. Metode matriks balikan");
         System.out.println("4. Kaidah Cramer");
+        System.out.println("");
     }
 }
