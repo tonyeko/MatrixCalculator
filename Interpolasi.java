571de4a72e0c8e8501ebb2edc57e7766f02dca31
tonyeko <tonyeko@users.noreply.github.com>
2019-09-20 20:14:44 +0700

    new method getcoef, getconst

---
+++ Interpolasi.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.*;
+
+class Interpolasi {
+
+    Interpolasi () {}
+
+    float XPangkat (float x, int n)
+    {
+       float hasil;
+       int i;
+
+       hasil = 1;
+        for (i=1;i<=n;i++)
+        {
+            hasil = hasil * x; 
+        }
+        return hasil;
+    }
+
+    Matriks InputTitik (int N)
+    {
+        Matriks M = new Matriks ();
+        M.NRowEff = N;
+        M.NColEff = N+1;
+        int i,j;
+        float x,y;
+        Scanner input = new Scanner(System.in);
+    
+        for (i=1;i<=N;i++)
+        {   
+            M.Mat[i][1] = 1;
+            x = input.nextFloat(); 
+            y = input.nextFloat();
+            M.Mat[i][N+1] =  y;
+            for(j=2;j<=N;j++)
+            {
+                M.Mat[i][j] =  XPangkat(x,(j-1));
+            }
+        }
+
+        return M;
+
+    }
+
+    
+}
+    
---
+++ Menu.java
@@ -0,0 +1,53 @@
+class Menu {
+
+    menu() {}
+
+    void ListMenu() {
+        System.out.println("");
+        System.out.println("MENU");
+        System.out.println("1. Sistem Persamaan Linear");
+        System.out.println("2. Determinan");
+        System.out.println("3. Matriks balikan");
+        System.out.println("4. Matriks kofaktor");
+        System.out.println("5. Adjoin");
+        System.out.println("6. Interpolasi Polinom");
+        System.out.println("7. Exit");
+        System.out.println("");
+    }
+    void SubMenu(){
+        System.out.println("1. Metode eliminasi Gauss");
+        System.out.println("2. Metode eliminasi Gauss-Jordan");
+        System.out.println("3. Metode matriks balikan");
+        System.out.println("4. Kaidah Cramer");
+        System.out.println("");
+    }
+
+    
+    void HasilMenu(int PilihanMenu) { 
+        if (PilihanMenu == 1)
+        {}
+        else if (PilihanMenu == 2)
+        {}
+        else if (PilihanMenu == 3)
+        {}
+        else if (PilihanMenu == 4)
+        {}
+        else if (PilihanMenu == 5)
+        {}
+        else if (PilihanMenu == 6)
+        {}
+
+    }
+
+    void HasilSubMenu(int PilihanSubMenu) { 
+        if (PilihanSubMenu == 1)
+        {}
+        else if (PilihanSubMenu == 2)
+        {}
+        else if (PilihanSubMenu == 3)
+        {}
+        else if (PilihanSubMenu == 4)
+        {}
+
+    }
+}
