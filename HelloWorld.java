95e3b14e84ebaf8ef527dfae36432e8936c33f7c
hengkysuryaa <hengkysurya88@gmail.com>
2019-09-20 19:21:56 +0700

    hello

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+// Mencetak Hello World!
+
+public class HelloWorld {
+
+    public static void main (String[] args) {
+
+        float x;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello world!");
+
+        x = input.nextFloat();
+        System.out.print(x);
+
+       
+    }
+
+
+}
