101c6bf1b0d78976053b818a21bd235c9f6ae5b6
hengkysuryaa <hengkysurya88@gmail.com>
2019-09-24 08:32:45 +0700

    menu fix

--- DriverMenu.java
+++ DriverMenu.java
@@ -4,44 +4,46 @@ class DriverMenu {
     public static void main(String[] args) {
 
         // deklarasi variabel
-        int PilihanMenu,PilihanSubMenu;
+       // int PilihanMenu,PilihanSubMenu;
 
         // membuat scanner baru
-        Scanner input = new Scanner(System.in);
+       // Scanner input = new Scanner(System.in);
 
         menu M = new menu ();
+        M.MainMenu();
 
-        do {
+    //     do {
             
-            M.ListMenu();
-            do {
-                System.out.print("Pilihan Anda: ");
-                PilihanMenu = input.nextInt();
-                if (PilihanMenu < 1 || PilihanMenu > 7)
-                {
-                    System.out.println("Menu tidak terdefinisi. Silakan masukkan kembali.");
-                    System.out.println("");
-                } 
-            } while ((PilihanMenu < 1 || PilihanMenu > 7));
-            M.HasilMenu(PilihanMenu);
+    //         M.ListMenu();
+    //         do {
+    //             System.out.print("Pilihan Anda: ");
+    //             PilihanMenu = input.nextInt();
+    //             if (PilihanMenu < 1 || PilihanMenu > 7)
+    //             {
+    //                 System.out.println("Menu tidak terdefinisi. Silakan masukkan kembali.");
+    //                 System.out.println("");
+    //             } 
+    //         } while ((PilihanMenu < 1 || PilihanMenu > 7));
+    //         M.HasilMenu(PilihanMenu);
 
-            if (PilihanMenu == 1 || PilihanMenu == 2 || PilihanMenu == 3)
-                {      
-                        System.out.println("");
-                        M.SubMenu();
-                        do 
-                        {
-                            System.out.print("Pilihan Anda: ");
-                            PilihanSubMenu = input.nextInt();
-                            if (PilihanSubMenu < 1 || PilihanSubMenu > 4)
-                             {
-                                 System.out.println("SubMenu tidak terdefinisi. Silakan masukkan kembali.");
-                                 System.out.println("");
-                             }
-                        } while ((PilihanSubMenu < 1 || PilihanSubMenu > 4));    
-                        M.HasilSubMenu(PilihanSubMenu);
-                }
+    //         if (PilihanMenu == 1 || PilihanMenu == 2 || PilihanMenu == 3)
+    //             {      
+    //                     System.out.println("");
+    //                     M.SubMenu();
+    //                     do 
+    //                     {
+    //                         System.out.print("Pilihan Anda: ");
+    //                         PilihanSubMenu = input.nextInt();
+    //                         if (PilihanSubMenu < 1 || PilihanSubMenu > 4)
+    //                          {
+    //                              System.out.println("SubMenu tidak terdefinisi. Silakan masukkan kembali.");
+    //                              System.out.println("");
+    //                          }
+    //                     } while ((PilihanSubMenu < 1 || PilihanSubMenu > 4));    
+    //                     M.HasilSubMenu(PilihanSubMenu);
+    //             }
 
-        } while (PilihanMenu != 7);
-    }
+    //     } while (PilihanMenu != 7);
+    // }
+}
 }
--- menu.java
+++ menu.java
@@ -1,10 +1,15 @@
+import java.io.*;
+import java.util.*;
+
 class menu {
 
     menu () {}
 
     void ListMenu() {
+     
         System.out.println("");
-        System.out.println("MENU");
+        System.out.println("=============== SPL, Determinan, dan Aplikasinya ===============");
+        System.out.println("============================= Menu =============================");
         System.out.println("1. Sistem Persamaan Linear");
         System.out.println("2. Determinan");
         System.out.println("3. Matriks balikan");
@@ -13,41 +18,153 @@ class menu {
         System.out.println("6. Interpolasi Polinom");
         System.out.println("7. Exit");
         System.out.println("");
+    
     }
-    void SubMenu(){
+
+    void HasilMenu(int PilihanMenu) { 
+        if (PilihanMenu == 1)
+        { this.SubMenuSPL(); }
+        else if (PilihanMenu == 2)
+        { this.SubMenuDeterminan(); }
+        else if (PilihanMenu == 3)
+        {  this.SubMenuInvers(); }
+        else if (PilihanMenu == 4)
+        { System.out.println("Kofaktor "); }
+        else if (PilihanMenu == 5)
+        { System.out.println("Adjoin");}
+        else if (PilihanMenu == 6)
+        { System.out.println("Interpolasi");}
+    }
+
+    void SubMenuSPL(){
+        System.out.println("");
+        System.out.println("=========================== SubMenuSPL ===========================");
         System.out.println("1. Metode eliminasi Gauss");
         System.out.println("2. Metode eliminasi Gauss-Jordan");
         System.out.println("3. Metode matriks balikan");
         System.out.println("4. Kaidah Cramer");
-        System.out.println("");
+ 
     }
 
     
-    void HasilMenu(int PilihanMenu) { 
-        if (PilihanMenu == 1)
-        {}
-        else if (PilihanMenu == 2)
-        {}
-        else if (PilihanMenu == 3)
-        {}
-        else if (PilihanMenu == 4)
-        {}
-        else if (PilihanMenu == 5)
-        {}
-        else if (PilihanMenu == 6)
-        {}
+    void SubMenuDeterminan(){
+        System.out.println("");
+        System.out.println("======================== SubMenuDeterminan ========================");
+        System.out.println("1. Metode Operasi Baris Elementer");
+        System.out.println("2. Metode Kofaktor");
+       
+}
+
+    void SubMenuInvers(){
+        System.out.println("");
+        System.out.println("====================== SubMenuMatriksBalikan ======================");
+        System.out.println("1. Metode Gauss-Jordan");
+        System.out.println("2. Metode Kofaktor");
+    
+    }
+
+    void HasilSubMenuSPL(int PilihanSubMenu) { 
+        if (PilihanSubMenu == 1)
+        {System.out.println("1. Metode eliminasi Gauss");}
+        else if (PilihanSubMenu == 2)
+        {System.out.println("1. Metode eliminasi Gauss");}
+        else if (PilihanSubMenu == 3)
+        {System.out.println("1. Metode eliminasi Gauss");}
+        else if (PilihanSubMenu == 4)
+        {System.out.println("1. Metode eliminasi Gauss");}
+    }
+
+    void HasilSubMenuDeterminan(int PilihanSubMenu) { 
+        if (PilihanSubMenu == 1)
+        {System.out.println("1. Metode Operasi Baris Elementer");}
+        else if (PilihanSubMenu == 2)
+        {System.out.println("1. Metode Operasi Baris Elementer");}
 
     }
 
-    void HasilSubMenu(int PilihanSubMenu) { 
+    void HasilSubMenuInvers(int PilihanSubMenu) { 
         if (PilihanSubMenu == 1)
-        {}
+        {System.out.println("1. Metode Operasi Baris Elementer");}
         else if (PilihanSubMenu == 2)
-        {}
-        else if (PilihanSubMenu == 3)
-        {}
-        else if (PilihanSubMenu == 4)
-        {}
+        {System.out.println("1. Metode Operasi Baris Elementer");}
+    }
 
+    
+
+    void MainMenu () 
+    {
+       // deklarasi variabel
+       int PilihanMenu,PilihanSubMenu;
+
+       // membuat scanner baru
+       Scanner input = new Scanner(System.in);
+
+        do {
+            this.ListMenu();
+            do {
+                System.out.print("Pilihan Anda: ");
+                PilihanMenu = input.nextInt();
+                if (PilihanMenu < 1 || PilihanMenu > 7)
+                {
+                    System.out.println("Menu tidak terdefinisi. Silakan masukkan kembali.");
+                    System.out.println("");
+                } 
+            } while ((PilihanMenu < 1 || PilihanMenu > 7));
+            this.HasilMenu(PilihanMenu);
+
+            if (PilihanMenu == 1 || PilihanMenu == 2 || PilihanMenu == 3)
+                {      
+                        System.out.println("");
+                        if (PilihanMenu == 1)
+                        {
+                            
+                            do 
+                        {
+                            System.out.print("Pilihan Anda: ");
+                            PilihanSubMenu = input.nextInt();
+                            if (PilihanSubMenu < 1 || PilihanSubMenu > 4)
+                             {
+                                 System.out.println("SubMenu tidak terdefinisi. Silakan masukkan kembali.");
+                                 System.out.println("");
+                             }
+                        } while ((PilihanSubMenu < 1 || PilihanSubMenu > 4));   
+                        this.HasilSubMenuSPL(PilihanSubMenu); 
+
+                        }
+                        else if ( PilihanMenu == 2)
+                        {
+                            do 
+                        {
+                            System.out.print("Pilihan Anda: ");
+                            PilihanSubMenu = input.nextInt();
+                            if (PilihanSubMenu < 1 || PilihanSubMenu > 2)
+                             {
+                                 System.out.println("SubMenu tidak terdefinisi. Silakan masukkan kembali.");
+                                 System.out.println("");
+                             }
+                        } while ((PilihanSubMenu < 1 || PilihanSubMenu > 2));   
+                        this.HasilSubMenuDeterminan(PilihanSubMenu);  
+
+                        }
+                        else if ( PilihanMenu == 3)
+                        {
+                            do 
+                        {
+                            System.out.print("Pilihan Anda: ");
+                            PilihanSubMenu = input.nextInt();
+                            if (PilihanSubMenu < 1 || PilihanSubMenu > 2)
+                             {
+                                 System.out.println("SubMenu tidak terdefinisi. Silakan masukkan kembali.");
+                                 System.out.println("");
+                             }
+                        } while ((PilihanSubMenu < 1 || PilihanSubMenu > 2));    
+                        this.HasilSubMenuInvers(PilihanSubMenu); 
+
+                        }
+                        
+                }
+
+        } while (PilihanMenu != 7);
     }
 }
+
